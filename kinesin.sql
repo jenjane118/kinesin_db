54036df3fd34d924804ef0a2d98bfede049f7699
Jennifer J. Stiens <j.j.stiens@gmail.com>
2019-01-22 13:26:16 +0000

    changed column lengths

--- kinesin.sql
+++ kinesin.sql
@@ -46,21 +46,20 @@ CREATE table										gene
 
 
 CREATE table										mutation
-(			genomic_id								VARCHAR(45)				NOT NULL,
+(			genomic  								VARCHAR(45)				NOT NULL,
             coding										CHAR(1)							NOT NULL,
             cds											VARCHAR(10)				DEFAULT 'N/A'
 																									NOT NULL,
-			mutation_type							VARCHAR(20)				DEFAULT 'UNK'
+			mutation_type							VARCHAR(100)				DEFAULT 'UNK'
 																									NOT NULL,
-			consequence							VARCHAR(20)				DEFAULT 'UNK'
+			consequence							VARCHAR(100)				DEFAULT 'UNK'
 																									NOT NULL,
-            protein										VARCHAR(10)				DEFAULT 'N/A'
-																									NOT NULL,
+            protein										VARCHAR(10)				NOT NULL,
 			gene_name								VARCHAR(100)				NOT NULL,
             organism									VARCHAR(100)				NOT NULL,
             domain									VARCHAR(100)				DEFAULT 'UNK'
 																									NOT NULL,
-            PRIMARY KEY (genomic_id),
+            PRIMARY KEY (protein),
             FOREIGN KEY (gene_name, organism) REFERENCES gene (gene_name, organism) ON DELETE CASCADE
 )ENGINE=InnoDB;
            
@@ -72,7 +71,7 @@ CREATE table										source_info
 																									NOT NULL,
             mutation_id								VARCHAR(45)				NOT NULL,
 			PRIMARY KEY (source_id),
-            FOREIGN KEY (mutation_id) REFERENCES mutation (genomic_id) ON DELETE CASCADE
+            FOREIGN KEY (mutation_id) REFERENCES mutation (protein) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 -- deleted description table and included in mutation table
@@ -100,7 +99,7 @@ CREATE table										frequency
 			gdc_freq									VARCHAR(10)				DEFAULT 'UNK'
 																									NOT NULL,
 			PRIMARY KEY (mutation_id),
-            FOREIGN KEY (mutation_id) REFERENCES mutation (genomic_id) ON DELETE CASCADE
+            FOREIGN KEY (mutation_id) REFERENCES mutation (protein) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 
@@ -125,7 +124,7 @@ CREATE table										impact
 			clinvar_prediction					VARCHAR(25)				DEFAULT 'UNK'
 																									NOT NULL,
 			PRIMARY KEY (mutation_id),
-            FOREIGN KEY (mutation_id) REFERENCES mutation (genomic_id) ON DELETE CASCADE
+            FOREIGN KEY (mutation_id) REFERENCES mutation (protein) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 CREATE table										tissue
@@ -135,18 +134,18 @@ CREATE table										tissue
 			cancer_type							VARCHAR(100)				DEFAULT 'UNK'
 																									NOT NULL,
 			PRIMARY KEY (mutation_id),
-            FOREIGN KEY (mutation_id) REFERENCES mutation (genomic_id) ON DELETE CASCADE
+            FOREIGN KEY (mutation_id) REFERENCES mutation (protein) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 
 -- materialised view created for user interface: includes only coding mutations for given gene (kinesin)
 -- in future can expand to create new views for each gene (kinesin) or for non-coding mutations (CNVs)
 CREATE VIEW vKif11_coding_mut AS 
-			SELECT 	g.gene_name, g.organism, m.genomic_id, cds, protein, mutation_type, consequence, domain, custom_score
+			SELECT 	g.gene_name, g.organism, m.genomic, cds, protein, mutation_type, consequence, domain, custom_score
             FROM 	gene g, mutation m, impact i
             WHERE 	m.coding = 'Y'
             AND		g.gene_name = m.gene_name
 			AND		g.organism = m.organism
- 			AND		m.genomic_id = i.mutation_id;
+ 			AND		protein = i.mutation_id;
             
 
