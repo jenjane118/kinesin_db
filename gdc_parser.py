2e6fb4af949ad3913024bee0a448ac1aa655552f
Jennifer J. Stiens <j.j.stiens@gmail.com>
2019-01-23 16:06:39 +0000

    Consolidated all parsing and inserts for mutation, impact and source tables

--- gdc_parser.py
+++ gdc_parser.py
@@ -26,9 +26,10 @@ gdc_parser         SELF
 
 Revision History:
 =================
-22.01.19        PyMysql insert data, create dictionary              JJS
-23.01.19        Rewrite into function (insertMutation)              JJS     
-
+V1.2    22.01.19        PyMysql insert data, create dictionary      By: JJS
+V1.3    23.01.19        Rewrite into function (insertMutation)          JJS     
+V1.4    23.01.19        Added functions for source and impact           JJS
+                        tables
 """
 
 # ******************************************************************************
@@ -85,18 +86,51 @@ def insertSource(mutations):
 
     cnx.commit()
     cnx.close()
+# ******************************************************************************
+
+def insertImpact(impact_list):
+    """Inserts entry into impact table.
+    Input               impact_list             list of entries for impact table
+    Output                                      inserted row into impact table
+    """
+    # Connect to MySQL database
+    cnx = pymysql.connect(host=config_kinesin.database_config['dbhost'],
+                          port=config_kinesin.database_config['port'],
+                          user=config_kinesin.database_config['dbuser'],
+                          passwd=config_kinesin.database_config['dbpass'],
+                          db=config_kinesin.database_config['dbname'])
+    cursor = cnx.cursor(pymysql.cursors.DictCursor)
+
+    # sql_impact = "INSERT INTO impact (mutation_id, custom_score, vep, sift_score, sift_prediction, " \
+    #              "polyphen_score, polyphen_prediction, fathhm_score, fathhm_prediction, " \
+    #              "clinvar_prediction) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    #
+    sql_impact = "INSERT INTO impact (mutation_id, vep, sift_prediction, polyphen_prediction) " \
+                 "VALUES(%s,%s,%s,%s)"
+
+
+    for x in impact_list:
+        if x[0] != "None":
+            rows = cursor.execute(sql_impact, x)
+
+    cnx.commit()
+    cnx.close()
 
 # ******************************************************************************
 def parseMutations(file):
     """ Function to parse json files from Genomic Data Commons.
     Input                   file                file of JSON mutations
-    Output                  mut_list            list of strings for mutation table
+    Output                  mut_list  [0]       list of strings for mutation table
+                            source_list  [1]    list of strings for source table
+                            impact_list  [2]    list of strings for impact table
     """
 
     mut_list    = []
     mut_entry   = []
     source_entry= []
     source_list = []
+    impact_entry = []
+    impact_list = []
 
     source_db   = 'GDC'
     my_gene     = 'KIF11'
@@ -114,6 +148,15 @@ def parseMutations(file):
             genomic_id      = genomic_id.replace('g.', '')
             source_id       = str(mutations[x]['ssm_id'])
             mutation_type   = str(mutations[x]['mutation_subtype'])
+            vep = str(mutations[x]['consequence'][0]['transcript']['annotation']['vep_impact'])
+            if vep == '':
+                vep = ' '
+            polyphen = str(mutations[x]['consequence'][0]['transcript']['annotation']['polyphen_impact'])
+            if polyphen == '':
+                polyphen = ' '
+            sift = str(mutations[x]['consequence'][0]['transcript']['annotation']['sift_impact'])
+            if sift == '':
+                sift = ' '
             x += 1
         except Error as e:
             print("Error", e)
@@ -121,10 +164,12 @@ def parseMutations(file):
         if gene_name == my_gene:            ## check that gene is KIF11
             mut_entry       = [genomic_id, 'y', ' ', mutation_type, consequence, protein, gene_name, 'homo sapiens']
             source_entry    = [source_id, source_db, transcript, protein]
+            impact_entry = [protein, vep, sift, polyphen]
+            impact_list.append(impact_entry)
             mut_list.append(mut_entry)
             source_list.append(source_entry)
 
-    return mut_list, source_list
+    return mut_list, source_list, impact_list
 
 # ******************************************************************************
 
@@ -135,13 +180,14 @@ if __name__ == "__main__":
     with open ('mutations.2018-10-03.json', 'r') as f:
         mutations = json.load(f)
 
-    gdc_mut = parseMutations(mutations)[0]
+    gdc_mut    = parseMutations(mutations)[0]
     gdc_source = parseMutations(mutations)[1]
-
+    gdc_impact = parseMutations(mutations)[2]
 
 
     insertMutation(gdc_mut)
     insertSource(gdc_source)
+    insertImpact(gdc_impact)
 
     i=0
     for k in gdc_mut:
