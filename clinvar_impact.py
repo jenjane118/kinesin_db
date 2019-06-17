c73cb7965134b2da16964b43141be411de648078
Jennifer J. Stiens <j.j.stiens@gmail.com>
2019-06-17 22:56:25 +0100

    debugged mutation identification

--- clinvar_impact.py
+++ clinvar_impact.py
@@ -41,6 +41,7 @@ import re
 import config_home
 import pymysql
 import config_kinesin
+import Bio.Data.IUPACData
 
 #*****************************************************************************
 
@@ -50,37 +51,46 @@ def parseClinvar(gene, csv_file):
                         csv_file            file of Clinvar results returned from web query
     Output              cv_list             list of mutation identifier and clinical significance
     """
-
+    #new_mutation = ''
+    #mut = ''
+    #clin_sig = ''
     cv_entry = []
     cv_list  = []
     # open csv file
     with open(csv_file, 'r') as file:
-        csv_reader = csv.reader(file, delimiter='\t')
-        first_row = True
+        csv_reader = csv.DictReader(file, delimiter='\t')
+        #first_row = True
         for row in csv_reader:
-            if first_row == True:           #skip first header row with titles
-                first_row = False
-            else:
-                try:
-                    gene_name   = row[1]
-                    genomic     = row[8]    #genomic location on chromosome 10
-                    # remove anything after first position number
-                    p       = re.compile(r'\s\-.*$')
-                    genomic = p.sub('', genomic)
-                    # reformat genomic entry to include chromosome number
-                    genomic     = '10:' + genomic
-                    clin_sig    = row[3]    #clinical significance
-                    # remove review date in parentheses
-                    q           = re.compile(r'\(.*\)$')
-                    clin_sig    = q.sub('', clin_sig)
-
-                except Exception as e:
-                    print("Error", e)
-
-                if gene_name == gene:       #check that gene is correct one
-                    cv_entry = [genomic, clin_sig]
+            clin_sig = row['Clinical significance (Last reviewed)']  # clinical significance
+            # remove review date in parentheses
+            r = re.compile(r'\(.*\)$')
+            clin_sig = r.sub('', clin_sig)
+            mut_name    = row['Name']
+                    ##NM_004523.3(KIF11):c.2T>C (p.Met1Thr)
+            p       = re.compile(r'.+(\(KIF11\)).+p.(\w+)')
+            it      = p.finditer(mut_name)
+            for match in it:
+                mut = str(match.group(2))
+                # extract 3-letter amino acid codes and resnum
+                q       = re.compile(r'(\D+)(\d+)(\D{3})')
+                ob      = q.finditer(mut)
+                for x in ob:
+                    old_aa3 = str(x.group(1))
+                    resnum = str(x.group(2))
+                    new_aa3 = str(x.group(3))
+                # reformat protein entry for one letter IUPAC code
+                    try:
+                        old_aa1 = Bio.Data.IUPACData.protein_letters_3to1[old_aa3]
+                    except KeyError:
+                        old_aa1 = '*'
+                    try:
+                        new_aa1 = Bio.Data.IUPACData.protein_letters_3to1[new_aa3]
+                    except KeyError:
+                        new_aa1 = '*'
+                    new_mutation = old_aa1 + resnum + new_aa1
+            #cv_list.append(new_mutation)
+                    cv_entry = [clin_sig, new_mutation]
                     cv_list.append(cv_entry)
-
     return cv_list
 
 # ******************************************************************************
@@ -109,23 +119,10 @@ def clinvarUpdate(clinvar_list, database):
 
     cursor = cnx.cursor(pymysql.cursors.DictCursor)
 
-    impact_list = []
-    # use genomic attribute to find protein(mutation_id) for impact table
-    query = "SELECT protein FROM mutation WHERE genomic = %s ;"
-    for x in clinvar_list:
-        mutation = str.format(x[0])         # retrieve mutation (genomic location) from parsed clinvar list
-        with cnx.cursor() as cursor:
-            cursor.execute(query, (mutation))
-            temp = cursor.fetchone()
-
-        x.pop(0)        # remove genomic location from first position
-        x.append(temp)  # add mutation_id (aa change) to last position
-        impact_list.append(x)
-
     sql_clinvar = "UPDATE impact SET clinvar_prediction = %s WHERE mutation_id = %s;"
 
     count = 0
-    for item in impact_list:
+    for item in clinvar_list:
         with cnx.cursor() as cursor:
             rows = cursor.execute(sql_clinvar, item)
             count += 1
@@ -147,7 +144,7 @@ if __name__ == "__main__":
     db = 'home'
 
     results = parseClinvar(mygene, myfile)
-    #print(results)
+    print(results)
 
     successful = clinvarUpdate(results, db)
     print(successful)
