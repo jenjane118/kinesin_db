68c169ed93bf5a9eb541923e86b112a39a9b5d4f
Jennifer J. Stiens <j.j.stiens@gmail.com>
2019-07-13 11:56:09 +0100

    analysis

--- sql_queries/tool_comparison.sql
+++ sql_queries/tool_comparison.sql
@@ -33,7 +33,7 @@ SELECT mutation_id, condel, cadd_rank, fathmm_cancer_pred
                   WHERE  cadd_rank >0.8   AND fathmm_cancer_pred = 'CANCER'
                   GROUP by mutation_id;
                   
-SELECT * from impact
+SELECT * from tissue
 WHERE mutation_id in ('L991V', 'F1012I', 'L500F', 'P1010Q');
 
 SELECT mutation_id, domain
@@ -43,3 +43,12 @@ SELECT mutation_id, domain
                   GROUP by mutation_id
                   ORDER by resnum ASC;
                   
+SELECT * FROM impact
+WHERE median_ranked_scores >=0.8;
+
+SELECT mutation_id, domain
+FROM kinesin.impact, kinesin.mutation
+                  WHERE condel > 0.8 AND cadd_rank>=0.8 AND metaSVM_rank>=0.8 AND revel_rank>=0.8 AND median_ranked_scores>=0.8
+                  AND mutation_id = protein
+                  GROUP by mutation_id
+                  ORDER by resnum ASC;
